c03b3bc3804a1070e58bbba86177e816a7bfb4a3
Monique <skaiwilliams85@gmail.com>
2023-06-06 03:29:53 -0500

    added media query

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,9 @@
 const signup = (req, res) => {
-  res.render("signup", { link: "/signup", text: "Login" });
+  res.render("signup", { link: "/login", text: "Login" });
 };
 
 const login = (req, res) => {
-  res.render("login", { link: "/login", text: "Sign Up" });
+  res.render("login", { link: "/signup", text: "Sign Up" });
 };
 
 module.exports = { signup, login };
